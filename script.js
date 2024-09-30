8293613aabad7d7cabf52e8a435583945bff69b3
Cole Flannery <jaidencoleflannery@gmail.com>
2024-09-29 21:50:38 -0500

    Some styling changes + typo fixes.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Menu Toggle
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
 const menuToggle = document.getElementById('menu-toggle');
 const dropdownMenu = document.querySelector('.menu-button .dropdown-menu');
+const titleGrid = document.getElementById('title-grid');
 
 menuToggle.addEventListener('click', () => {
     dropdownMenu.classList.toggle('active');
 });
+
+for(let height = 0; height < screenHeight; height += 100){
+    for(let width = 0; width < screenWidth; width += 100){
+        titleGrid.innerHTML += `<div class='title-tile'></div>`;
+    }
+}
