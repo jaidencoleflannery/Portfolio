c3dde277bc7e1b98cd18fc617618edb53258f037
Cole Flannery <jaidencoleflannery@gmail.com>
2024-09-30 01:43:24 -0500

    Finished routing functionality.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Menu Toggle
-const screenWidth = window.screen.width;
-const screenHeight = window.screen.height;
 const menuToggle = document.getElementById('menu-toggle');
 const dropdownMenu = document.querySelector('.menu-button .dropdown-menu');
-const titleGrid = document.getElementById('title-grid');
 
 menuToggle.addEventListener('click', () => {
     dropdownMenu.classList.toggle('active');
 });
+
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const titleGrid = document.getElementById('title-grid');
 
 for(let height = 0; height < screenHeight; height += 60){
     for(let width = 0; width < screenWidth; width += 60){
@@ -15,8 +15,18 @@ for(let height = 0; height < screenHeight; height += 60){
     }
 }
 
+const homeRedirect = document.getElementById('home-redirect');
 const aboutRedirect = document.getElementById('about-redirect');
+const contactRedirect = document.getElementById('contact-redirect');
+
+homeRedirect.addEventListener('click', () => {
+    window.location.replace('file:///Users/coleflannery/Documents/Webdev/Projects/Portfolio/index.html');
+});
 
 aboutRedirect.addEventListener('click', () => {
     window.location.replace('file:///Users/coleflannery/Documents/Webdev/Projects/Portfolio/about.html');
+});
+
+contactRedirect.addEventListener('click', () => {
+    window.location.replace('file:///Users/coleflannery/Documents/Webdev/Projects/Portfolio/contact.html');
 });
